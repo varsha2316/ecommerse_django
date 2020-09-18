06cddbe46869e46a321ccddfc5f9c1063258607a
varsha2316 <varshachothy@gmail.com>
2020-09-18 19:47:43 +0000

    fixed static files

--- ecommerse_django/settings.py
+++ ecommerse_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://varsha-ecommerse.herokuapp.com.', 'localhost']
+ALLOWED_HOSTS = ['https://varsha-ecommerse.herokuapp.com', 'localhost']
 
 
 # Application definition
